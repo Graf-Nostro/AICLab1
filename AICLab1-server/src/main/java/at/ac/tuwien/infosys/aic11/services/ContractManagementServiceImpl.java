0fe27783ab301aa93ea484e3113228be2b80809b
wokung <kung.wong@gmail.com>
2011-11-09 21:55:40 +0100

    tried to add ws-security with this tutorial http://www.jroller.com/gmazza/entry/cxf_x509_profile but still not running

--- AICLab1-server/src/main/java/at/ac/tuwien/infosys/aic11/services/ContractManagementServiceImpl.java
+++ AICLab1-server/src/main/java/at/ac/tuwien/infosys/aic11/services/ContractManagementServiceImpl.java
@@ -1,8 +1,14 @@
 package at.ac.tuwien.infosys.aic11.services;
 
 import javax.jws.WebService;
+import org.apache.cxf.interceptor.InInterceptors;
 
 @WebService( serviceName = "contractWS" )
 public class ContractManagementServiceImpl implements ContractManagementService, AbstractWebService {
-
+	
+	
+	@Override
+	public String testEncryption(String s) {
+		return "This is a test, if you can read this, this is bad, " + s + "!";
+	}
 }
