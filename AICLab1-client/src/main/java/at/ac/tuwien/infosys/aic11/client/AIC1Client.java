d64dbc5bd3b527e2c9c5f01374dea9daea5cd585
wokung <kung.wong@gmail.com>
2011-11-13 18:06:41 +0100

    still unfinished ws-security, because of a connectexecption still unfinished ws-security, because of a connectexecption

--- AICLab1-client/src/main/java/at/ac/tuwien/infosys/aic11/client/AIC1Client.java
+++ AICLab1-client/src/main/java/at/ac/tuwien/infosys/aic11/client/AIC1Client.java
@@ -1,6 +1,5 @@
 package at.ac.tuwien.infosys.aic11.client;
 
-<<<<<<< HEAD
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,6 +34,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import at.ac.tuwien.infosys.aic11.cfg.JettyConfig;
 import at.ac.tuwien.infosys.aic11.cfg.WicketConfig;
 import at.ac.tuwien.infosys.aic11.cfg.XmlConfig;
+import at.ac.tuwien.infosys.aic11.dto.Address;
+import at.ac.tuwien.infosys.aic11.dto.BankTransfer;
+import at.ac.tuwien.infosys.aic11.dto.Cheque;
+import at.ac.tuwien.infosys.aic11.dto.CreditRequest;
+import at.ac.tuwien.infosys.aic11.dto.Customer;
+import at.ac.tuwien.infosys.aic11.dto.CustomerRating;
+import at.ac.tuwien.infosys.aic11.dto.DisbursementPreference;
+import at.ac.tuwien.infosys.aic11.dto.Duration;
+import at.ac.tuwien.infosys.aic11.dto.InterestRate;
+import at.ac.tuwien.infosys.aic11.dto.Money;
+import at.ac.tuwien.infosys.aic11.dto.Offer;
+import at.ac.tuwien.infosys.aic11.dto.Query;
+import at.ac.tuwien.infosys.aic11.dto.QueryResponse;
+import at.ac.tuwien.infosys.aic11.dto.Rating;
+import at.ac.tuwien.infosys.aic11.dto.Warrantor;
+import at.ac.tuwien.infosys.aic11.dto.WsdlEndpoint;
 
 
 import org.apache.cxf.frontend.ClientProxy;
@@ -46,36 +61,7 @@ import org.apache.ws.security.handler.WSHandlerConstants;
 public class AIC1Client {
 	public static void main(String[] args) throws Exception {
 
-		/** WS SECURITY */
-		/**
-		// Configure Spring
-		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("client-context.xml"));
-		ContractManagementService port = (ContractManagementService) factory.getBean("clientkey");
-		
-		// Configure Interceptors outgoing properties
-				
-		org.apache.cxf.endpoint.Client clientEndpoint = ClientProxy.getClient(port);
-		org.apache.cxf.endpoint.Endpoint cxfEndpoint = clientEndpoint.getEndpoint();
-		
-		Map<String,Object> inProps= new HashMap<String,Object>();
-		// how to configure the properties is outlined below;
-
-		WSS4JInInterceptor wssIn = new WSS4JInInterceptor(inProps);
-		cxfEndpoint.getInInterceptors().add(wssIn);
-
-		Map<String,Object> outProps = new HashMap<String,Object>();
-	    // how to configure the properties is outlined below;
-
-		WSS4JOutInterceptor wssOut = new WSS4JOutInterceptor(outProps);
-		cxfEndpoint.getOutInterceptors().add(wssOut);
-		
-		outProps.put(WSHandlerConstants.ACTION, "Signature Encryption");
-		outProps.put(WSHandlerConstants.USER, "clientkey");
-		outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, 
-		    ClientKeystorePasswordCallback.class.getName());
-		outProps.put(WSHandlerConstants.SIG_PROP_FILE, "clientKeystore.properties");
-		*/
-		
+			
 //		TestService               testWS    = getWebService( TestService.class );
 //		RatingService             ratings   = getRestService( RatingService.class );
 //		IRegistryService          registry  = getWebService( IRegistryService.class, "http://vc.infosys.tuwien.ac.at:8092/registry" );
@@ -88,19 +74,20 @@ public class AIC1Client {
 		
 //		client.getRequestContext().put("schema-validation-enabled", "true"); 
 				
-		Object cheque   = ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Cheque" )
-                                        .setProperty( "name", "Fader A. Vader" )
-                                        .build();
-		Object money    = ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Money" )
-		                                .setProperty( "currencyCode", "EUR" )
-		                                .setProperty( "amount", 5000 )
-		                                .build();
-		Object customer = ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Customer" )
-		                                .setProperty( "address",
-		                                	ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Address" )
-		                                	              .build()
-		                                )
-		                                .build();
+//		Object cheque   = ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Cheque" )
+//                                        .setProperty( "name", "Fader A. Vader" )
+//                                        .build();
+//		Object money    = ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Money" )
+//		                                .setProperty( "currencyCode", "EUR" )
+//		                                .setProperty( "amount", 5000 )
+//		                                .build();
+//		Object customer = ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Customer" )
+//		                                .setProperty( "address",
+//	                                	ReflectionUtil.makeInstance( "at.ac.tuwien.infosys.aic11.services.Address" )
+//		                                	              .build()
+//		                                )
+//		                                .build();
+		
 		
 //		Object[] result = client.invoke(
 //			"start_money_transfer_process",
@@ -129,7 +116,11 @@ public class AIC1Client {
 	
 //		publishWebServices();
 //		org.apache.cxf.endpoint.Server restServer = publishRestServices();
-	
+		
+		// Get a handle to the service via CXF generated client
+		ContractManagementService cmService = getWebService( ContractManagementService.class );
+		System.out.println("Message received: " + cmService.testEncryption2());
+		
 		httpServer.join();
 //	 restServer.destroy();
 	}	
@@ -206,35 +197,47 @@ public class AIC1Client {
 ////		println( registry.query( new Cheque() ).getLocation() );
 //	}
 //	
-//	public static <E> E getWebService( Class<E> serviceClass ) {
-//		Config cfg = new Config();
-//		
-//		return getWebService( 
-//			serviceClass, 
-//			String.format(
-//				"http://localhost:%d/%s",
-//				cfg.getInt("http.server.port.ws"),
-//				Services.getServiceName( serviceClass )
-//			)
-//		);
-//	}
-//	public static <E> E getWebService( Class<E> serviceClass, String address ) {
-//		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-//		
-//		JAXBDataBinding jaxb = new JAXBDataBinding();
-////		jaxb.setExtraClass( new Class[] {Addresses.class, BankTransfer.class, Cheque.class, CreditRequest.class,
-////				Customer.class, CustomerRating.class, DisbursementPreference.class, Duration.class, InterestRate.class, Money.class, Offer.class, Query.class,
-////				QueryResponse.class, Rating.class, Warrantor.class, WsdlEndpoint.class} );
-//		
-//		factory.setDataBinding( jaxb );
-//		
-//		factory.getInInterceptors().add( new LoggingInInterceptor() );
-//		factory.getOutInterceptors().add( new LoggingOutInterceptor() );
-//
-//		factory.setAddress( address );
-//
-//		return (E) factory.create( serviceClass );
-//	}
+	public static <E> E getWebService( Class<E> serviceClass ) {
+		Config cfg = new Config();
+		
+		return getWebService( 
+			serviceClass, 
+			String.format(
+				"http://localhost:%d/%s",
+				cfg.getInt("http.server.port.ws"),
+				Services.getServiceName( serviceClass )
+			)
+		);
+	}
+	public static <E> E getWebService( Class<E> serviceClass, String address ) {
+		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+		
+		JAXBDataBinding jaxb = new JAXBDataBinding();
+		jaxb.setExtraClass( new Class[] {Address.class, BankTransfer.class, Cheque.class, CreditRequest.class,
+				Customer.class, CustomerRating.class, DisbursementPreference.class, Duration.class, InterestRate.class, Money.class, Offer.class, Query.class,
+				QueryResponse.class, Rating.class, Warrantor.class, WsdlEndpoint.class} );
+		
+		factory.setDataBinding( jaxb );
+		
+		factory.getInInterceptors().add( new LoggingInInterceptor() );
+		factory.getOutInterceptors().add( new LoggingOutInterceptor() );
+		// ALREADY ADDED THROUGH CLIENT-CONTENT.XML
+// 		Map<String,Object> inProps= new HashMap<String,Object>();
+//		Map<String,Object> outProps = new HashMap<String,Object>();
+//	    // how to configure the properties is outlined below;
+//		outProps.put(WSHandlerConstants.ACTION, "Timestamp Signature Encrypt");
+//		outProps.put(WSHandlerConstants.USER, "clientkey");
+//		outProps.put(WSHandlerConstants.PW_CALLBACK_CLASS, 
+//		    ClientKeystorePasswordCallback.class.getName());
+//		outProps.put(WSHandlerConstants.SIG_PROP_FILE, "clientKeystore.properties");
+//   	factory.getInInterceptors().add( new WSS4JInInterceptor(inProps) ); 
+//		factory.getOutInterceptors().add( new WSS4JOutInterceptor(outProps) );
+		
+	
+		factory.setAddress( address );
+
+		return (E) factory.create( serviceClass );
+	}
 //	public static <E> E getRestService( Class<E> serviceClass ) {
 //		Config cfg = new Config();
 //		
